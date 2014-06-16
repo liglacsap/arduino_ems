9cfa2758f4b6fe3826792faac5e97533f606aff0
gil <gil@gil-ThinkPad-E520>
2014-06-16 10:38:34 +0200

    Erste Idee Übertragungsprotokoll

---
+++ EMSCommunicator.h
@@ -0,0 +1,17 @@
+/*
+ * EMSCommunicator.h
+ *
+ *  Created on: 02.06.2014
+ *      Author: gil
+ */
+
+#ifndef EMSCOMMUNICATOR_H_
+#define EMSCOMMUNICATOR_H_
+
+class EMSCommunicator {
+public:
+	EMSCommunicator();
+	virtual ~EMSCommunicator();
+};
+
+#endif /* EMSCOMMUNICATOR_H_ */
---
+++ Release/EMSCommunicator.cpp.d
@@ -0,0 +1,4 @@
+EMSCommunicator.cpp.d EMSCommunicator.cpp.o: ../EMSCommunicator.cpp \
+  ../EMSCommunicator.h
+
+../EMSCommunicator.h:
