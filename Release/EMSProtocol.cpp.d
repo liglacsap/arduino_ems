e28af781012e01bce4d41ff2d1c3220b2338e9da
gil <fernwaerts@gmail.com>
2014-07-16 13:13:20 +0200

    avxc

---
+++ Release/EMSProtocol.cpp.d
@@ -0,0 +1,39 @@
+EMSProtocol.cpp.d EMSProtocol.cpp.o: ../EMSProtocol.cpp ../EMSProtocol.h \
+  /home/gil/Downloads/eclipseArduino/workspace/test/McpDigitalPot/McpDigitalPot.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Arduino.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/binary.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/WCharacter.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/WString.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Stream.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Print.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Printable.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/new.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/USBAPI.h \
+  /home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/variants/standard/pins_arduino.h
+
+../EMSProtocol.h:
+
+/home/gil/Downloads/eclipseArduino/workspace/test/McpDigitalPot/McpDigitalPot.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Arduino.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/binary.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/WCharacter.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/WString.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/HardwareSerial.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Stream.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Print.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/Printable.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/new.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/cores/arduino/USBAPI.h:
+
+/home/gil/Downloads/arduino-1.5.6-r2/hardware/arduino/avr/variants/standard/pins_arduino.h:
